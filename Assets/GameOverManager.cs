c08173b3b2ae952f4939ab43539ec821a6a5e4c7
matiasflorentinacosta-droid <matiasflorentinacosta@gmail.com>
2026-04-23 17:48:25 -0300

    UI
    PANTALLA PAUSA, MENU PRINCIPAL, GAME OVER,VICTORIA,

---
+++ Assets/GameOverManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public GameObject gameOverPanel;
+
+    void Update()
+    {
+        // Presioná K para probar el Game Over
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SampleScene");
+    }
+}
